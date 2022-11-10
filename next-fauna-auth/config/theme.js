071d79225053a41f43200bfc424a731dd8ac6d83
Higashi-Kota <higashi.kota@gri.jp>
2022-11-10 13:42:33 +0900

    moved by rename

--- next-fauna-auth/config/theme.js
+++
@@ -1,9 +0,0 @@
-import '@fontsource/inter';
-import {extendTheme} from '@mui/joy/styles';
-const customTheme = extendTheme({
-  fontFamily: {
-    body: 'Inter, sans-serif',
-  },
-});
-
-export {customTheme};
