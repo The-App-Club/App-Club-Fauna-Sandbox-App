6505b6da8653bcc13730a99e7330a371dc105e7a
Higashi-Kota <higashi.kota@gri.jp>
2023-01-03 17:19:51 +0900

    wip save

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { withSuperjson } = require('next-superjson')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withSuperjson()(nextConfig)
